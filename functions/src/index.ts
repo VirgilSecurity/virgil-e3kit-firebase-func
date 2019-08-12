f7ac95cf4c0d478a95781e65c7ceb72d543a3c77
Vadim Avdeev <v.avdeiev@gmail.com>
2019-08-12 18:20:42 +0300

    fix import

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,4 @@
-import functions from 'firebase-functions';
+import * as functions from 'firebase-functions';
 import admin from 'firebase-admin';
 import { app } from './app';
 import { generateVirgilJwt } from './generate-virgil-jwt';
