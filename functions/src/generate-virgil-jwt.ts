14c3130e32cfb98b273afde5e2a1cd960ee21107
Dmytro Kosolap <42dkosolap@gmail.com>
2020-04-01 12:53:06 +0300

    update dependency, remove tests and express

--- functions/src/generate-virgil-jwt.ts
+++ functions/src/generate-virgil-jwt.ts
@@ -1,18 +1,23 @@
 import { JwtGenerator } from 'virgil-sdk';
-import { VirgilCrypto, VirgilAccessTokenSigner } from 'virgil-crypto';
+import { initCrypto, VirgilCrypto, VirgilAccessTokenSigner } from 'virgil-crypto';
 import * as functions from 'firebase-functions';
 
-const crypto = new VirgilCrypto();
+async function getJwtGenerator() {
+  await initCrypto();
+  
+  const crypto = new VirgilCrypto();
+  
+  const { app_id, app_key_id, app_key } = functions.config().virgil;
 
-const { app_id, app_key_id, app_key } = functions.config().virgil;
+  return new JwtGenerator({
+    appId: app_id,
+    apiKeyId: app_key_id,
+    apiKey: crypto.importPrivateKey(app_key),
+    accessTokenSigner: new VirgilAccessTokenSigner(crypto),
+  });
+}
 
-const generator = new JwtGenerator({
-  appId: app_id,
-  apiKeyId: app_key_id,
-  apiKey: crypto.importPrivateKey(app_key),
-  accessTokenSigner: new VirgilAccessTokenSigner(crypto)
-});
-
-export function generateVirgilJwt(identity: string) {
+export async function generateVirgilJwt(identity: string) {
+  const generator = await getJwtGenerator();
   return generator.generateToken(identity);
-} 
+}
